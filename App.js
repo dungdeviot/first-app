5f69aa531aa5100c23a935865f16e19112fe549a
Nguyen Van Quoc Dung <dungnguyen1052k@gmail.com>
2022-05-17 14:20:48 +0700

    React Native 10

--- App.js
+++ App.js
@@ -1,81 +1,104 @@
 import React from 'react';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const Separator = () => (
-  <View style={styles.separator} />
-);
+const App = () => {
+  return (
+    <View style={styles.container}>
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-        The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
-      </Text>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
-      </Text>
-      <Button
-        title="Press me"
-        color="#f194ff"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        All interaction for the component are disabled.
-      </Text>
-      <Button
-        title="Press me"
-        disabled
-        onPress={() => Alert.alert('Cannot press this one')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        This layout strategy lets the title define the width of the button.
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          onPress={() => Alert.alert('Left button pressed')}
-        />
-        <Button
-          title="Right button"
-          onPress={() => Alert.alert('Right button pressed')}
-        />
+      <View style={styles.containerTop}>
+        <View style={styles.top}>
+          <Text style={styles.topText}>Top Left</Text>
+        </View>
+      </View>
+        
+
+      <View style={styles.containerCenter}>
+        <View style={styles.center}>
+          <Text style={styles.centerText}>Center</Text>
+        </View>
+      </View>
+
+      <View style={styles.containerBottom}>
+        <View style={styles.bottom}>
+          <Text style={styles.bottomText}>Bottom Right</Text>
+        </View>
       </View>
     </View>
-  </SafeAreaView>
-);
+  )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
+  container : {
+    backgroundColor : '#E8F8F7',
+    flex : 1
   },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
+
+  containerTop : {
+    backgroundColor : 'green',
+    flex : 1
   },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+
+  top : {
+    marginHorizontal: 20,
+    marginTop : 25,
+    paddingHorizontal : 10,
+    paddingVertical : 10,
+    backgroundColor : '#FAF3FA',
+    borderWidth : 3,
+    borderColor : 'red'
   },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+  
+  topText: {
+    fontSize : 20,
+    color : '#3390FF'
   },
+
+  containerCenter : {
+    backgroundColor : 'red',
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center'
+  },
+  
+  center : {
+    width : 150,
+    height : 150,
+    backgroundColor : '#FAF3FA',
+    borderRadius : 100,     //Bo tròn background
+    justifyContent : 'center',  //Canh giữa chiều dọc
+    alignItems : 'center'   //Canh giữa chiều ngang
+  },
+  
+  centerText: {
+    fontSize : 20,
+    color : '#3390FF',
+    fontWeight : 'bold'   //In đậm chữ
+  },
+
+  containerBottom : {
+    backgroundColor : 'yellow',
+    flex : 1
+  },
+  
+  bottom : {
+    marginHorizontal: 20,
+    marginTop : 170,
+    paddingHorizontal : 10,
+    paddingVertical : 10,
+    backgroundColor : '#FAF3FA',
+    borderWidth : 3,
+    borderColor : 'red',
+    borderRadius : 22     //Bo tròn background
+    
+  },
+  
+  bottomText: {
+    textAlign : 'right',
+    fontSize : 20,
+    color : '#3390FF',
+    fontWeight : 'bold'   //In đậm chữ
+  }
 });
 
 export default App;
